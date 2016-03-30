ce8da3e069acfe7fd352109d6a38671cdad3d1a0
Sonny <sk222sw@student.lnu.se>
2016-03-30 23:07:33 +0200

    added react-router

--- app/index.js
+++ app/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
+
+import Layout from "./pages/Layout";
+import Playlist from "./pages/Playlist";
 
 import "./stylesheets/style.css";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const app = document.getElementById('app');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Layout}>
+      <IndexRoute component={App}></IndexRoute>
+      <Route path="playlist" component={Playlist}></Route>
+    </Route>
+  </Router>,
+app);
---
+++ app/pages/Playlist.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class Playlist extends React.Component {
+  render() {
+    return (
+      <h1>Playlist</h1>
+    );
+  }
+}
