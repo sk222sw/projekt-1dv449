f3fa65939bcff3b0ecc7cd9d13f29e1f2ce27531
Sonny <sk222sw@student.lnu.se>
2016-03-31 23:04:21 +0200

    moved database things to DAL

---
+++ models/DAL.js
@@ -0,0 +1,62 @@
+const Promise = require("bluebird");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://sonny:123@ds037165.mlab.com:37165/playlist")
+
+const Schema = mongoose.Schema;
+const PlaylistSchema = new Schema({
+  title: String,
+  tracks: Array
+});
+
+const Playlist = mongoose.model("PlaylistSchema", PlaylistSchema);
+
+const DAL = function () {};
+
+DAL.prototype.getPlaylist = function() {
+  return new Promise((resolve, reject) => {
+    Playlist.find({_id: "56e972b23f5229f01a57b7a8"}, (err, json) => {
+      if (err) { reject(err); }
+      resolve(json);
+    });
+  });
+};
+
+DAL.prototype.addTrack = function (playlistId, track) {
+  return new Promise((resolve, reject) => {
+    Playlist.findById(playlistId, (err, playlist) => {
+      if (err) { reject(err); }
+      playlist.tracks.push(track);
+      playlist.save(err => {
+        if (err) { reject(err); }
+        else {
+          return true;
+        }
+      })
+    })
+  });
+}
+
+DAL.prototype.deleteTrack = function (playlistId, track) {
+    return new Promise((resolve, reject) => {
+      Playlist.findById(playlistId, (err, playlist) => {
+        if (err) { reject(err); }
+
+        var trackIndex = null;
+        for (var i = 0; i < playlist.tracks.length; i++) {
+          if (playlist.tracks[i].id === track) {
+            trackIndex = i;
+          }
+        }
+        playlist.tracks.splice(trackIndex, 1);
+        playlist.save(function (err) {
+           if(err) { reject(err); }
+           return true;
+        });
+      })
+    })
+}
+
+DAL.prototype.createPlaylist = function() {};
+
+module.exports = new DAL();
--- routes/playlist.js
+++ routes/playlist.js
@@ -1,86 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const Promise = require("bluebird");
-const mongoose = require("mongoose");
+const DAL = require("./../models/DAL");
+
 const _ = require("lodash");
 
-mongoose.connect("mongodb://sonny:123@ds037165.mlab.com:37165/playlist")
 
-const Schema = mongoose.Schema;
-const PlaylistSchema = new Schema({
-  title: String,
-  tracks: Array
-});
-
-const Playlist = mongoose.model("PlaylistSchema", PlaylistSchema);
 
 router.get('/', (req, res) => {
 
 });
 
 router.get('/:id', (req, res) => {
-  getPlaylist()
+  DAL.getPlaylist()
   .then(json => {
     res.json(json);
   })
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  const playlistId = req.body.playlistId;
-  return new Promise((resolve, reject) => {
-    Playlist.findById(playlistId, (err, playlist) => {
-      if (err) { reject(err); }
-      playlist.tracks.push(req.body.track);
-      playlist.save(err => {
-        if (err) { reject(err); }
-        else {
-          res.send("succeeded");
-        }
-      })
-    })
-  });
+  DAL.addTrack(req.body.playlistId, req.body.track)
+  .then(r => {
+    if (r) {
+      res.send("succeeded");
+    }
+  })
+  .catch(err => {
+    console.log(err.message);
+    res.send("Sorry, there was an error adding the track to the database");
+  })
 })
 
 router.post("/:id/delete/:track", (req, res) => {
   const playlistId = req.params.id;
   const track = req.params.track;
 
-  // console.log("track",track);
+  DAL.deleteTrack(req.params.id, req.params.track)
+  .then(r => {
+    if (r) {
+      res.send(`dios mio!!`);
+    }
+  })
 
-  new Promise((resolve, reject) => {
-    Playlist.findById(playlistId, (err, playlist) => {
-      if (err) { reject(err); }
-
-      var trackIndex = null;
-      for (var i = 0; i < playlist.tracks.length; i++) {
-        if (playlist.tracks[i].id === track) {
-          trackIndex = i;
-        }
-      }
-      playlist.tracks.splice(trackIndex, 1);
-      playlist.save(function (err) {
-         if(err) { reject(err); }
-         res.send("deleted");
-      });
-    })
-  })
-  .catch((err) => {
-    console.log("error");
-  })
-  .then(() => {
-    res.send(`dios mio!!`);    
-  })
 });
 
-function getPlaylist() {
-  return new Promise((resolve, reject) => {
-    Playlist.find({_id: "56e972b23f5229f01a57b7a8"}, (err, json) => {
-      if (err) { reject(err); }
-      resolve(json);
-    });
-  });
-};
+
 
 
 module.exports = router;
